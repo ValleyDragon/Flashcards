b9c481e5605dca3006550faaa2f374f990e56802
ValleyDragon888 <ValleyDragon888@gmail.com>
2025-09-10 06:39:28 +0100

    start to add yaml

--- project/new/card.gd
+++ project/new/card.gd
@@ -7,14 +7,21 @@ var flashcards = {
 	]
 }
 
+func load_flashcards(path: String):
+	var file = FileAccess.open(path, FileAccess.READ)
+	var content = file.get_as_text()
+	var yaml_data = YAML.parse(content).get_data()
+	var converted = convert_flashcards(yaml_data)
+	return converted
+
 func convert_flashcards(raw:Dictionary) -> Array:
-	var included_flashcard_types = raw.keys()
+	var included_flashcard_types = raw["cards"].keys()
 	var converted = []
 	for type in included_flashcard_types:
-		var cards = raw[type]
+		var cards = raw["cards"][type]
 		for card in cards:
 			converted.append({"type": type, "data": card})
 	return converted
 
 func _ready() -> void:
-	print(convert_flashcards(flashcards))
+	print(load_flashcards("res://flashcards/test.yaml"))
